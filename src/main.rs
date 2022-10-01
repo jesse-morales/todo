c3ec7419ce88d3aee875fa2abc4b6734e8c40b85
jesse-morales <moral.jesse@outlook.com>
2022-10-01 16:27:43 -0600

    Add task adding capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::{OpenOptions}, io::{Write}};
+use rocket::serde::{Deserialize, json::Json};
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -8,4 +10,24 @@ fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
+}
+
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+                    .read(true)
+                    .append(true)
+                    .create(true)
+                    .open("tasks.txt")
+                    .expect("unable to access tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
+    "Task added succesfully"
 }
